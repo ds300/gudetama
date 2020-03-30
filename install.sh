ba9878b4bb69d40d8b6012010fa49c0968b5a4d6
artsyit <it@artsymail.com>
2020-03-30 14:34:02 +0000

    Update install script [skip ci]

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
   set -e
   mkdir -p ~/.gudetama/bin
   echo '#!/usr/bin/env node' > ~/.gudetama/bin/gudetama
-  curl https://raw.githubusercontent.com/artsy/gudetama/f5b6d61c4bb982fce3c5060e4f175022f848f9bc/dist.js >> ~/.gudetama/bin/gudetama
+  curl https://raw.githubusercontent.com/artsy/gudetama/bfd59dd42ae71b8463d6d7bd506c4be64170cc0f/dist.js >> ~/.gudetama/bin/gudetama
   chmod +x ~/.gudetama/bin/gudetama
   export PATH=$PATH:~/.gudetama/bin
