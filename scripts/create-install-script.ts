5a3e56decf980c4484fa5b16016dbe9e79bd91d4
David Sheldrick <d.j.sheldrick@gmail.com>
2020-03-30 10:36:25 +0100

    set -e in install script

--- scripts/create-install-script.ts
+++ scripts/create-install-script.ts
@@ -6,6 +6,7 @@ const currentCommit = execSync('git rev-parse HEAD').toString().trim()
 fs.writeFileSync(
   'install.sh',
   `
+  set -e
   mkdir -p ~/.gudetama/bin
   echo '#!/usr/bin/env node' > ~/.gudetama/bin/gudetama
   curl https://raw.githubusercontent.com/artsy/gudetama/${currentCommit}/dist.js >> ~/.gudetama/bin/gudetama
