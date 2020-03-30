ece580a8d2758887feccf77f38732dc6a0a49c4f
David Sheldrick <d.j.sheldrick@gmail.com>
2020-03-30 16:19:16 +0100

    fix gudetama install

--- scripts/create-install-script.ts
+++ scripts/create-install-script.ts
@@ -11,7 +11,14 @@ fs.writeFileSync(
   echo '#!/usr/bin/env node' > ~/.gudetama/bin/gudetama
   curl https://raw.githubusercontent.com/artsy/gudetama/${currentCommit}/dist.js >> ~/.gudetama/bin/gudetama
   chmod +x ~/.gudetama/bin/gudetama
-  export PATH=$PATH:~/.gudetama/bin
+  if [[ -z "$BASH_ENV" ]]
+  then
+    >&2 echo 'Cannot find BASH_ENV environment variable'
+    exit 1
+  else
+    echo 'export PATH=$PATH:~/.gudetama/bin' >> "$BASH_ENV"
+  fi
+  echo -e 'gudetama successfuly installed at ~/.gudetama/bin/gudetama'
 `,
   { mode: 33261 }
 )
