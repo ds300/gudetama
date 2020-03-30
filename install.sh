705f1fd068f1fc4f266f73b34fbe236b25a52da1
artsyit <it@artsymail.com>
2020-03-30 15:25:29 +0000

    Update install script [skip ci]

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
   set -e
   mkdir -p ~/.gudetama/bin
   echo '#!/usr/bin/env node' > ~/.gudetama/bin/gudetama
-  curl https://raw.githubusercontent.com/artsy/gudetama/e44439c31359c7a520df486214e8c0f5a1ba79f8/dist.js >> ~/.gudetama/bin/gudetama
+  curl https://raw.githubusercontent.com/artsy/gudetama/c864bebc4b4826bf7aadbce8e8aa7448717a395a/dist.js >> ~/.gudetama/bin/gudetama
   chmod +x ~/.gudetama/bin/gudetama
-  export PATH=$PATH:~/.gudetama/bin
+  if [[ -z "$BASH_ENV" ]]
+  then
+    >&2 echo 'Cannot find BASH_ENV environment variable'
+    exit 1
+  else
+    echo 'export PATH=$PATH:~/.gudetama/bin' >> "$BASH_ENV"
+  fi
+  echo -e 'gudetama successfuly installed at ~/.gudetama/bin/gudetama'
