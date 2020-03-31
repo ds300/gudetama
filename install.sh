5686ac9a3b45e9edf376bd2061e44219499e3c63
artsyit <it@artsymail.com>
2020-03-31 11:50:18 +0000

    Update install script [skip ci]

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
   set -e
   mkdir -p ~/.gudetama/bin
   echo '#!/usr/bin/env node' > ~/.gudetama/bin/gudetama
-  curl https://raw.githubusercontent.com/artsy/gudetama/99f9226fdee7acd9535dc80fb0cf61267021111c/dist.js >> ~/.gudetama/bin/gudetama
+  curl https://raw.githubusercontent.com/artsy/gudetama/10aae3fbe05fa90bdce06763c8f07837aa93238e/dist.js >> ~/.gudetama/bin/gudetama
   chmod +x ~/.gudetama/bin/gudetama
   if [[ -z "$BASH_ENV" ]]
   then
