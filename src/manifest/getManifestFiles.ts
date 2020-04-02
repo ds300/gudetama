1e3816987f8955e6a63923815dae6b4abe002691
David Sheldrick <d.j.sheldrick@gmail.com>
2020-04-02 20:34:12 +0100

    support dir matching for inputFiles

--- src/manifest/getManifestFiles.ts
+++ src/manifest/getManifestFiles.ts
@@ -1,5 +1,7 @@
 import { getStep } from '../config'
 import glob from 'glob'
+import fs from 'fs'
+import path from 'path'
 
 export function getManifestFiles({ stepName }: { stepName: string }) {
   const { inputFiles } = getStep({ stepName })
@@ -11,15 +13,34 @@ export function getManifestFiles({ stepName }: { stepName: string }) {
     }
   }
   for (const globPattern of inputFiles?.include ?? []) {
-    for (const file of glob.sync(globPattern, { nodir: true })) {
-      files.add(file)
+    for (const file of glob.sync(globPattern)) {
+      if (fs.statSync(file).isDirectory()) {
+        visitAllFiles(file, (filePath) => files.add(filePath))
+      } else {
+        files.add(file)
+      }
     }
   }
   for (const globPattern of inputFiles?.exclude ?? []) {
-    for (const file of glob.sync(globPattern, { nodir: true })) {
-      files.delete(file)
+    for (const file of glob.sync(globPattern)) {
+      if (fs.statSync(file).isDirectory()) {
+        visitAllFiles(file, (filePath) => files.delete(filePath))
+      } else {
+        files.delete(file)
+      }
     }
   }
 
   return [...files]
+}
+
+function visitAllFiles(dir: string, visit: (filePath: string) => void) {
+  for (const fileName of fs.readdirSync(dir)) {
+    const fullPath = path.join(dir, fileName)
+    if (fs.statSync(fullPath).isDirectory()) {
+      visitAllFiles(fullPath, visit)
+    } else {
+      visit(fullPath)
+    }
+  }
 }
