5dbe4434890da07f25a88e3224d7f0c57a08ad9f
David Sheldrick <d.j.sheldrick@gmail.com>
2020-04-02 12:53:18 +0100

    fix things

--- scripts/build-bundle.sh
+++ scripts/build-bundle.sh
@@ -2,6 +2,6 @@ set -e
 
 yarn rollup --config
 version=$(node -e 'console.log(require("./package.json").version)')
-filename=gudetama.$version.bundle.js
+filename=gudetama.v$version.js
 echo '#!/usr/bin/env node' > $filename
 cat dist/cli.js >> $filename
