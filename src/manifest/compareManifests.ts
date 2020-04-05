b6a230cf02c2e2ec3cd2dca3fa62d832719cfd3a
David Sheldrick <d.j.sheldrick@gmail.com>
2020-04-05 18:43:15 +0100

    remove debagging

--- src/manifest/compareManifests.ts
+++ src/manifest/compareManifests.ts
@@ -21,21 +21,14 @@ export function compareManifests({
 
   let i = 0
   let j = 0
-  let safety = 0
 
   while (i < previousLines.length || j < currentLines.length) {
-    if (safety++ > 100) {
-      throw new Error('bad times')
-    }
-    console.log('__comparing__ i', i, 'j', j)
-    console.log(previousLines[i], currentLines[j])
     const [previousThing, previousHash] = (previousLines[i] || '').split('\t')
     const [currentThing, currentHash] = (currentLines[j] || '').split('\t')
     if (previousThing === currentThing) {
       i++
       j++
       if (previousHash !== currentHash) {
-        console.log('different')
         changes.push(cyan(`${bold(currentThing)} is different`))
       }
     } else if (
@@ -43,11 +36,9 @@ export function compareManifests({
       (previousThing < currentThing || (previousThing && !currentThing))
     ) {
       i++
-      console.log('i removed')
       changes.push(red(`${bold(previousThing)} was removed`))
     } else {
       j++
-      console.log('j added')
       changes.push(green(`${bold(currentThing)} was added`))
     }
   }
