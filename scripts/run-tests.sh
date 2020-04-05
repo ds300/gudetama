a0994cfb4748c57594feacb46d6651b0ff1d12c6
David Sheldrick <d.j.sheldrick@gmail.com>
2020-04-05 18:26:03 +0100

    floob

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,7 +1,7 @@
 set -e
 
-# yarn babel-node src/cli run-if-needed prepare-tests --silent
-scripts/prepare-tests.sh
+yarn babel-node src/cli run-if-needed prepare-tests --silent
+# scripts/prepare-tests.sh
 
 export PATH=$(echo $PWD/test-bin:$PATH)
 node node_modules/.bin/jest "$@"
