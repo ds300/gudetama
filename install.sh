f448acd46065a364af5f1b3d9e2afbc0563e7eae
artsyit <it@artsymail.com>
2020-09-08 02:49:27 +0000

    Release v1.0.0-25 [skip ci]

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 set -e
 mkdir -p ~/.gudetama/bin
-curl -s -L https://github.com/artsy/gudetama/releases/download/v1.0.0-24/gudetama.v1.0.0-24.js > ~/.gudetama/bin/gudetama
+curl -s -L https://github.com/artsy/gudetama/releases/download/v1.0.0-25/gudetama.v1.0.0-25.js > ~/.gudetama/bin/gudetama
 chmod +x ~/.gudetama/bin/gudetama
 if [[ -z "$BASH_ENV" ]]
 then
